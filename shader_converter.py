cfc41ad36a510ad90f8b9b7ba3be561df4459ae6
Daniel Stokes <kupomail@gmail.com>
2017-03-02 19:34:54 -0800

    Clean up whitespace in shader_converter.py

--- shader_converter.py
+++ shader_converter.py
@@ -4,25 +4,41 @@ import gpu
 
 
 def vs_to_130(data):
-    data['attributes'].append({'varname': 'bl_Vertex', 'type': gpu.CD_ORCO, 'datatype': gpu.GPU_DATA_4F})
-    data['attributes'].append({'varname': 'bl_Normal', 'type': -1, 'datatype': gpu.GPU_DATA_3F})
-    data['uniforms'].append( {
+    data['attributes'].append({
+        'varname': 'bl_Vertex',
+        'type': gpu.CD_ORCO,
+        'datatype': gpu.GPU_DATA_4F
+    })
+    data['attributes'].append({
+        'varname': 'bl_Normal',
+        'type': -1,
+        'datatype': gpu.GPU_DATA_3F
+    })
+    data['uniforms'].append({
         'varname': 'bl_ModelViewMatrix',
         'type': 0,
         'datatype': gpu.GPU_DATA_16F,
     })
-    data['uniforms'].append( {
+    data['uniforms'].append({
         'varname': 'bl_ProjectionMatrix',
         'type': 0,
         'datatype': gpu.GPU_DATA_16F,
     })
-    data['uniforms'].append( {
+    data['uniforms'].append({
         'varname': 'bl_NormalMatrix',
         'type': 0,
         'datatype': gpu.GPU_DATA_9F,
     })
-    src = data['vertex']
-    src = '#version 130\nin vec4 bl_Vertex;\nin vec3 bl_Normal;\nuniform mat4 bl_ModelViewMatrix;\nuniform mat4 bl_ProjectionMatrix;\nuniform mat3 bl_NormalMatrix;\n' + src
+
+    src = '#version 130\n'
+    src += 'in vec4 bl_Vertex;\n'
+    src += 'in vec3 bl_Normal;\n'
+    src += 'uniform mat4 bl_ModelViewMatrix;\n'
+    src += 'uniform mat4 bl_ProjectionMatrix;\n'
+    src += 'uniform mat3 bl_NormalMatrix;\n'
+
+    src += data['vertex']
+
     src = re.sub(r'#ifdef USE_OPENSUBDIV([^#]*)#endif', '', src)
     src = re.sub(r'#ifndef USE_OPENSUBDIV([^#]*)#endif', r'\1', src)
     src = re.sub(r'#ifdef CLIP_WORKAROUND(.*?)#endif', '', src, 0, re.DOTALL)
@@ -33,8 +49,16 @@ def vs_to_130(data):
 
 
 def fs_to_130(data):
-    src = data['fragment']
-    src = '#version 130\nout vec4 frag_color;\nuniform mat4 bl_ProjectionMatrix;\nuniform mat4 bl_ModelViewMatrix;\nuniform mat4 bl_ModelViewMatrixInverse;\nuniform mat3 bl_NormalMatrix;\nuniform mat4 bl_ProjectionMatrixInverse;\n' + src
+    src = '#version 130\n'
+    src += 'out vec4 frag_color;\n'
+    src += 'uniform mat4 bl_ProjectionMatrix;\n'
+    src += 'uniform mat4 bl_ModelViewMatrix;\n'
+    src += 'uniform mat4 bl_ModelViewMatrixInverse;\n'
+    src += 'uniform mat3 bl_NormalMatrix;\n'
+    src += 'uniform mat4 bl_ProjectionMatrixInverse;\n'
+
+    src += data['fragment']
+
     src = re.sub(r'\bvarying\b', 'in', src)
     src = re.sub(r'\bgl_FragColor\b', 'frag_color', src)
     src = re.sub(r'\bgl_(?!FrontFacing)(.*?)\b', r'bl_\1', src)
@@ -71,7 +95,7 @@ def vs_to_web(data):
     src = data['vertex']
 
     precision_block = '\n'
-    for data_type in ('float','int'):
+    for data_type in ('float', 'int'):
         precision_block += 'precision mediump {};\n'.format(data_type)
 
     src = src.replace('#version 130', '#version 100\n' + precision_block)
@@ -85,10 +109,13 @@ def fs_to_web(data):
     src = data['fragment']
 
     precision_block = '\n'
-    for data_type in ('float','int'):
+    for data_type in ('float', 'int'):
         precision_block += 'precision mediump {};\n'.format(data_type)
 
-    src = src.replace('#version 130', '#version 100\n#extension GL_OES_standard_derivatives: enable\n' + precision_block)
+    header = '#version 100\n'
+    header += '#extension GL_OES_standard_derivatives: enable\n'
+    header += precision_block
+    src = src.replace('#version 130', header)
     src = re.sub(r'\bin\b', 'varying', src)
     src = src.replace('out vec4 frag_color;\n', '')
     src = re.sub(r'\bfrag_color\b', 'gl_FragColor', src)
