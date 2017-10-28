3ca8e70f24cc1e1888044d21c7a09b6f3ae5f51c
Daniel Stokes <kupomail@gmail.com>
2017-10-28 12:02:43 -0700

    Improve performance of unused data pruning
    Replacing the type checking and dynamic list operations with a
    tag and filter approach improved the export time of an object
    heavy scene from about 2 seconds to about 0.2 seconds.

--- filters.py
+++ filters.py
@@ -31,69 +31,54 @@ def used_only(bpy_data):
     Filter out the data blocks not in use by the objects in the list
     """
 
-    pruned_data = {
-        'actions': bpy_data['actions'],
-        'cameras': [],
-        'lamps': [],
-        'images': [],
-        'materials': [],
-        'meshes': [],
-        'objects': [],
-        'scenes': [],
-        'textures': [],
-    }
+    # Clear flags
+    for data_list in bpy_data.values():
+        for data in data_list:
+            data.tag = False
 
-    def check_object(obj):
-        # add object to list
-        pruned_data['objects'].append(obj)
+    # Always mark actions as used
+    for action in bpy_data['actions']:
+        action.tag = True
 
-        # handle dupli-groups
+    # Need a function to handle some recursion for dupli-groups
+    def tag_object(obj):
+        obj.tag = True
+
         if obj.dupli_group:
             for dupli_obj in obj.dupli_group.objects:
-                check_object(dupli_obj)
+                tag_object(dupli_obj)
 
-        # add scene to list
         for scene in obj.users_scene:
-            if scene not in pruned_data['scenes']:
-                pruned_data['scenes'].append(scene)
+            scene.tag = True
 
-        # add cameras to list
-        if isinstance(obj.data, bpy.types.Camera):
-            pruned_data['cameras'].append(obj.data)
+        if obj.data:
+            obj.data.tag = True
 
-        # add lights to list
-        elif isinstance(obj.data, bpy.types.Lamp):
-            pruned_data['lamps'].append(obj.data)
+        materials = []
+        if obj.data and hasattr(obj.data, 'materials'):
+            materials = obj.data.materials.values()
 
-        # add meshes to list
-        elif isinstance(obj.data, bpy.types.Mesh):
-            pruned_data['meshes'].append(obj.data)
+        for material in materials:
+            material.tag = True
 
-            # add materials to list
-            for mat in obj.data.materials.values():
-                if not mat:
-                    continue
+            textures = [
+                slot.texture for slot in material.texture_slots.values()
+                if slot is not None
+                and slot.use
+                and isinstance(slot.texture, bpy.types.ImageTexture)
+            ]
+            for texture in textures:
+                texture.tag = True
 
-                if mat not in pruned_data['materials']:
-                    pruned_data['materials'].append(mat)
+                if texture.image:
+                    texture.image.tag = True
 
-                    # add textures to list
-                    textures = [
-                        slot.texture for slot in mat.texture_slots.values()
-                        if slot is not None
-                        and slot.use
-                        and isinstance(slot.texture, bpy.types.ImageTexture)
-                    ]
-                    for tex in textures:
-                        if tex not in pruned_data['textures']:
-                            pruned_data['textures'].append(tex)
-
-                        # add images to list
-                        if tex.image not in pruned_data['images']:
-                            pruned_data['images'].append(tex.image)
-
-    # get list of objects
+    # Now let's go through the objects and start tagging
     for obj in bpy_data['objects']:
-        check_object(obj)
+        tag_object(obj)
+
+    pruned_data = {
+        key: [data for data in value if data.tag] for key, value in bpy_data.items()
+    }
 
     return pruned_data
